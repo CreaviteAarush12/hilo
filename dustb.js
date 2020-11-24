ca0223b0521223d19d5cf035a1112de4d2381d9e
CreaviteAarush12 <72918056+CreaviteAarush12@users.noreply.github.com>
2020-11-24 23:01:33 +0530

    Add files via upload

---
+++ dustb.js
@@ -0,0 +1,44 @@
+class dustb {
+
+}
+
+constructor (x,y)
+{
+
+this.x=x;
+this.y=y
+
+this.wall1 = bodies.rectangle(this.x+this.y, dustbinWidth/2,this.y-this.dustbinHeight/2,)
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
